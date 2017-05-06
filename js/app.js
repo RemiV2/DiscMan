f96195d227cc59cf59968b5364d5414e68f13185
Rémi de Juvigny <remidejuvigny@gmail.com>
2017-05-06 16:25:28 +0200

    Displays songs meta data in cards

--- js/app.js
+++ js/app.js
@@ -22,6 +22,8 @@ function openFile() {
   });
 }
 
+var card;
+
 function checkLibrary() {
   musicLibrary = JSON.parse(localStorage.getItem('musicLibrary'));
   if (musicLibrary.length > 0) {
@@ -30,5 +32,60 @@ function checkLibrary() {
     $('.content').show();
   }
   console.log(musicLibrary);
-  displayMusic();
+  //displayMusic();
+
+  for (i=0; i<musicLibrary.length; i++) {
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = "arraybuffer";
+    xhr.open("get", musicLibrary[i], true);
+    let j = i; // avoid closure problem where only last iteration is executed
+    xhr.onload = function(e) {
+
+      showMetaData(e.target.response, j);
+      //displayMusic(i);
+    }
+    xhr.send();
+  }
 }
+
+function showMetaData(data, index) {
+  musicmetadata(data, function (err, result) {
+    card = "<div class='card' id='" + index + "'> <div class='art'></div> <div class='info'> <p class='title'>Song name</p> <p class='details'>Album Name</p> </div> </div>";
+    if (err) throw err;
+    console.log(result);
+    // create song card and show details
+    $('#songsTab').append(card);
+    $('#songsTab #' + index + ' .title').html(result.title); // song name
+    $('#songsTab #' + index + ' .details').html(result.artist[0]); // artist
+    if (result.picture.length > 0) {
+      // add album cover art
+      var picture = result.picture[0];
+      var url = URL.createObjectURL(new Blob([picture.data], {'type': 'image/' + picture.format}));
+      $('#songsTab #' + index + ' .art').css({
+        'background': 'url(' + url + ')',
+        'background-size': 'cover'
+      });
+    }
+  });
+}
+
+// function displayMusic(index) {
+//   displaySongs(index);
+//   displayAlbums(index);
+//   displayArtists(index);
+// }
+
+// function displaySongs(index) {
+//   // code
+//   $('#songsTab').append(card);
+//   $('#songsTab #' + index + '.title').html = JSON.stringify(result, undefined, 2);
+//
+// }
+//
+// function displayAlbums(index) {
+//   // code
+// }
+//
+// function displayArtists(index) {
+//   // code
+// }
--- js/dragndrop-min.js
+++ js/dragndrop-min.js
@@ -45,3 +45,5 @@ function traverseFileTree(item, path) {
     });
   }
 }
+
+
