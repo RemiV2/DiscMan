6855956d79b199a58b694f4235ec9f3d080bf2a2
Rémi de Juvigny <remidejuvigny@gmail.com>
2018-04-03 11:53:42 +0200

    :sparkles: display first start message if user has no music

--- js/app.js
+++ js/app.js
@@ -1,14 +1,21 @@
 const ElectronStore = require('electron-store')
 const store = new ElectronStore()
 
+
 const tabs = document.querySelectorAll('header li.tab')
+const firstStart = document.querySelector('.first-start')
 
 import displayContent from './helpers/displayContent.js'
 import switchTab from './helpers/switchTab.js'
 import '../sass/style.sass'
 
-// Populate app with music library
-displayContent()
+if (!store.get('library')) {
+  // Manage first start
+  firstStart.classList.add('active')
+} else {
+  // Populate app with music library
+  displayContent()
+}
 
 // Switch tab on tab click
 for (const tab of tabs) {
