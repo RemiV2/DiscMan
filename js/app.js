00417384d6c183209d84917b3b632ef96302d75b
Rémi de Juvigny <remidejuvigny@gmail.com>
2018-04-03 10:48:04 +0200

    :sparkles: fill tabs with placeholder cards

--- js/app.js
+++ js/app.js
@@ -2,11 +2,7 @@ const ElectronStore = require('electron-store')
 const store = new ElectronStore()
 
 const tabs = document.querySelectorAll('header li.tab')
-const titlesSection = document.querySelector('.content#titles')
-const albumsSection = document.querySelector('.content#albums')
-const artistsSection = document.querySelector('.content#artists')
 
-import createSongCard from './components/createSongCard.js'
 import displayContent from './helpers/displayContent.js'
 import switchTab from './helpers/switchTab.js'
 import '../sass/style.sass'
@@ -19,7 +15,5 @@ for (const tab of tabs) {
   })
 }
 
-// for (let i = 0; i < 3; i++) {
-//   const card = createSongCard()
-//   content.appendChild(card)
-// }
+// Populate app with music library
+displayContent()
--- js/helpers/displayContent.js
+++ js/helpers/displayContent.js
@@ -1,6 +1,33 @@
+import createCard from './createCard.js'
 // Display content depending on the active tab
-displayContent = () => {
+export default () => {
+  // Get sections from DOM
+  const titlesSection = document.querySelector('.content[data-section="titles"]')
+  const albumsSection = document.querySelector('.content[data-section="albums"]')
+  const artistsSection = document.querySelector('.content[data-section="artists"]')
+
+  // Create fake card
+  // TODO: replace with real metadata
+  const cardInfo = {
+    title: 'Unknown Title',
+    details: 'Unknown Source',
+    src: '#',
+    cover: 'unknown.png'
+  }
+
   // Display titles
+  for (let i = 0; i < 20; i++) {
+    const card = createCard()
+    titlesSection.appendChild(card)
+  }
   // Display albums
+  for (let i = 0; i < 20; i++) {
+    const card = createCard()
+    albumsSection.appendChild(card)
+  }
   // Display artists
+  for (let i = 0; i < 20; i++) {
+    const card = createCard()
+    artistsSection.appendChild(card)
+  }
 }
